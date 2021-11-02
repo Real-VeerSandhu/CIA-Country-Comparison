25506fedb1e71cdfcb347d6ae5367890304fc1d3
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-11-01 21:42:34 -0400

    run app

--- app.py
+++ app.py
@@ -6,17 +6,21 @@ st.set_page_config(page_title="CIA Country Comparison", page_icon="🔎", layout
 st.write('# CIA Country Comparison')
 st.markdown('Compare data between two countries based on information from the CIA Factbook')
 
-data = pd.read_csv('data/cia_factbook.csv')
+def main():
+    data = pd.read_csv('data/cia_factbook.csv')
 
+    col1, col2 = st.columns(2)
 
-col1, col2 = st.columns(2)
+    with col1:
+        country1 = st.selectbox('Select Country #1', data['country'], index=2)
+        if country1:
+            st.markdown(f'## {country1}')
+            st.write(data)
 
-with col1:
-    country1 = st.selectbox('Select Country #1', data['country'])
-    if country1:
-        st.markdown(f'## {country1}')
+    with col2:
+        country2 = st.selectbox('Select Country #2', data['country'], index=1)
+        if country2:
+            st.markdown(f'## {country2}')
 
-with col2:
-    country2 = st.selectbox('Select Country #2', data['country'])
-    if country1:
-        st.markdown(f'## {country1}')
+if __name__ == '__main__':
+    main()
